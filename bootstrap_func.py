b98819ba0939f24ba1f21594dc00d3ed27cc0502
belinda <bwenke@caltech.edu>
2017-06-22 13:02:39 -0700

    Updated bootstrap

--- bootstrap_func.py
+++ bootstrap_func.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #Load data
 bd_1975 = np.loadtxt('data/beak_depth_scandens_1975.csv')
 bd_2012 = np.loadtxt('data/beak_depth_scandens_2012.csv')
@@ -7,12 +9,6 @@ def bs_replicate(data, func=np.mean):
     bs_sample = np.random.choice(data, replace=True, size=len(data))
     return func(bs_sample)
 
-#Generate lots of replicates
-n_reps = 100000
-
-#Initialize replicates array
-bs_reps_2012 = np.empty(n_reps)
-
-#Compute replicates
-for i in range(n_reps):
-    bs_reps_2012[i] = bs_replicate(bd_2012, func=np.mean)
+def draw_bs_reps(data, func=np.mean, size=10000):
+    """Draw bootstrap replicates from 1d data."""
+    return np.array([bs_replicate(data, func=func) for _ in range(size)])
